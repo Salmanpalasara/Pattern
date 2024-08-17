f4d9d0bf9424c279109179605edfcc28683b723d
Salman Palasara <salmanpalasara2202@gmail.com>
2024-08-17 09:52:37 +0530

    pattern

---
+++ pr.py
@@ -0,0 +1,677 @@
+
+# n = int(input("Enter a number of row :- "))
+
+# for i in range(n+1):
+# 	for j in range(i):
+# 		print("*", end= " ")
+# 	print()
+
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+
+
+# n = int(input("Enter a number of row :- "))
+
+# for i in range(n,0,-1):
+# 	for j in range(i):
+# 		print("*",end=" ")
+# 	print()
+
+# * * * * * * * *
+# * * * * * * *
+# * * * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+# n = int(input("Enter a number of row :- "))
+
+# for i in range(n):
+# 	for j in range(n,i,-1):
+# 		print(" ",end="")
+# 	for k in range(i+1):
+# 		print("*",end="")
+# 	print()
+
+# Enter a number of row :- 9
+#          *
+#         **
+#        ***
+#       ****
+#      *****
+#     ******
+#    *******
+#   ********
+#  *********
+
+# for i in range(5+1):
+# 	for j in range(i):
+# 		print(" ", end= " ")
+
+# 	for k in range(5,i-1,-1):
+# 		print("*",end = " ")
+
+# 	for m in range(5,i,-1):
+# 		print("*",end = " ")
+# 	print()
+
+# * * * * * * * * * * *
+#   * * * * * * * * *
+#     * * * * * * *
+#       * * * * *
+#         * * *
+#           *
+
+# n = int(input("Enter a number of row :- "))
+# for i in range(n):
+# 	for j in range(n,i,-1):
+# 		print(" ",end= "")
+
+# 	for k in range(i+1):
+# 		print("*",end="")
+
+# 	for m in range(i):
+# 		print("*",end="")
+# 	print()
+
+
+
+ #        *
+ #       ***
+ #      *****
+ #     *******
+ #    *********
+ #   ***********
+ #  *************
+ # ***************
+
+# n = int(input("Enter a number of row :- "))
+# for i in range(n):
+# 	for j in range(i+1):
+# 		print("*",end="")
+
+# 	for k in range(n-1,i,-1):
+# 		print(" ",end="")
+
+# 	for m in range(n,i+1,-1):
+# 		print(" ",end="")
+
+# 	for l in range(i+1):
+# 		print("*",end="")
+# 	print()
+
+# Enter a number of row :- 9
+# *                *
+# **              **
+# ***            ***
+# ****          ****
+# *****        *****
+# ******      ******
+# *******    *******
+# ********  ********
+# ******************
+
+
+
+# n = int(input("Enter a number of row :- "))
+# for i in range(n+1):
+# 	for j in range(i):
+# 		print("*",end= " ")
+# 	print()
+
+# for k in range(n-1,0,-1):
+# 	for m in range(k):
+# 		print("*",end=" ")
+# 	print()
+
+# Enter a number of row :- 8
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * * * *
+# * * * * * * *
+# * * * * * * * *
+# * * * * * * *
+# * * * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+
+# n = int(input("Enter a number of row :- "))
+# for i in range(n):
+# 	for j in range(n,i,-1):
+# 		print("*",end="")
+
+# 	for k in range(i):
+# 		print(" ",end="")
+
+# 	for l in range(i):
+# 		print(" ",end="")
+
+# 	for m in range(n,i,-1):
+# 		print("*",end="")
+# 	print()
+
+# Enter a number of row :- 8
+# ****************
+# *******  *******
+# ******    ******
+# *****      *****
+# ****        ****
+# ***          ***
+# **            **
+# *              *
+
+
+# n = int(input("Enter a number of row :- "))
+# for i in range(n):
+# 	for j in range(n,i,-1):
+# 		print(" ",end="")
+# 	for k in range(i+1):
+# 		print("* ",end="")
+# 	print()
+
+# Enter a number of row :- 8
+#         *
+#        * *
+#       * * *
+#      * * * *
+#     * * * * *
+#    * * * * * *
+#   * * * * * * *
+#  * * * * * * * *
+
+# n = int(input("Enter a number of row :- "))
+# for i in range(n):
+# 	for j in range(i+1):
+# 		print(" ",end="")
+
+# 	for k in range(n,i,-1):
+# 		print("* ",end="")
+# 	print()
+
+# Enter a number of row :- 9
+#  * * * * * * * * *
+#   * * * * * * * *
+#    * * * * * * *
+#     * * * * * *
+#      * * * * *
+#       * * * *
+#        * * *
+#         * *
+#          *
+
+
+# n = int(input("Enter a number of row :- "))
+# for i in range(n):
+# 	for j in range(n,i,-1):
+# 		print(" ",end="")
+
+# 	for k in range(i+1):
+# 		print("*",end="")
+# 	print()
+
+# for i in range(n):
+# 	for j in range(i+2):
+# 		print(" ",end="")
+
+# 	for k in range(n-1,i,-1):
+# 		print("*",end="")
+# 	print()
+
+# Enter a number of row :- 9
+#          *
+#         **
+#        ***
+#       ****
+#      *****
+#     ******
+#    *******
+#   ********
+#  *********
+#   ********
+#    *******
+#     ******
+#      *****
+#       ****
+#        ***
+#         **
+#          *
+
+# n = int(input("Enter a number  of row :- "))
+# for i in range(n):
+# 	for j in range(n,i,-1):
+# 		print(" ",end="")
+
+# 	for j in range(i+1):
+# 		print("*",end="")
+
+# 	for j in range(i):
+# 		print("*",end="")
+# 	print()
+
+# for i in range(n):
+# 	for j in range(i+2):
+# 		print(" ",end="")
+
+# 	for j in range(n-1,i,-1):
+# 		print("*",end="")
+
+# 	for j in range(n-2,i,-1):
+# 		print("*",end="")
+
+# 	print()
+
+# Enter a number  of row :- 9
+#          *
+#         ***
+#        *****
+#       *******
+#      *********
+#     ***********
+#    *************
+#   ***************
+#  *****************
+#   ***************
+#    *************
+#     ***********
+#      *********
+#       *******
+#        *****
+#         ***
+#          *
+
+# n = int(input("Enter a number of row :- "))
+# for i in range(n):
+# 	for j in range(n,i,-1):
+# 		print(" ",end="")
+
+# 	for j in range(i+1):
+# 		print("* ",end="")
+# 	print()
+
+# for i in range(n):
+# 	for j in range(i+2):
+# 		print(" ",end="")
+
+# 	for j in range(n-1,i,-1):
+# 		print("* ",end="")
+# 	print()
+
+# Enter a number of row :- 9
+#          *
+#         * *
+#        * * *
+#       * * * *
+#      * * * * *
+#     * * * * * *
+#    * * * * * * *
+#   * * * * * * * *
+#  * * * * * * * * *
+#   * * * * * * * *
+#    * * * * * * *
+#     * * * * * *
+#      * * * * *
+#       * * * *
+#        * * *
+#         * *
+#          *
+
+# n = int(input("Enter a Number of row :- "))
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ",end="")
+
+#     for j in range(n,i,-1):
+#         print("*",end="")
+
+#     for j in range(n-1,i,-1):
+#         print("*",end="")
+#     print()
+
+
+# for i in range(n):
+#     for j in range(n,i,-1):
+#         print(" ",end="")
+
+#     for j in range(i+1):
+#         print("*",end="")
+
+#     for j in range(i):
+#         print("*",end="")
+#     print()
+
+# Enter a Number of row :- 8
+#  ***************
+#   *************
+#    ***********
+#     *********
+#      *******
+#       *****
+#        ***
+#         *
+#         *
+#        ***
+#       *****
+#      *******
+#     *********
+#    ***********
+#   *************
+#  ***************
+
+# n = int(input("Enter a Number of row :- "))
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ",end="")
+
+#     for j in range(n,i,-1):
+#         print("* ",end="")
+
+#     print()
+# for i in range(n):
+#     for j in range(n,i,-1):
+#         print(" ",end="")
+
+#     for j in range(i+1):
+#         print("* ",end="")
+
+#     print()
+
+# Enter a Number of row :- 8
+#  * * * * * * * *
+#   * * * * * * *
+#    * * * * * *
+#     * * * * *
+#      * * * *
+#       * * *
+#        * *
+#         *
+#         *
+#        * *
+#       * * *
+#      * * * *
+#     * * * * *
+#    * * * * * *
+#   * * * * * * *
+#  * * * * * * * *
+
+# n = int(input("Enter a Number of row :- "))
+
+# for i in range(n):
+#     for j in range(i+1):
+#         if(j%2==0):
+#             print("0",end="")
+#         else:
+#             print("1",end="")
+#     print()
+
+# Enter a Number of row :- 8
+# 0
+# 01
+# 010
+# 0101
+# 01010
+# 010101
+# 0101010
+# 01010101
+
+# n = int(input("Enter a Number of row :- "))
+
+# for i in range(n):
+#     for j in range(i+1):
+#         if((i + j)%2==0):
+#             print("0",end="")
+#         else:
+#             print("1",end="")
+#     print()
+
+# 0
+# 10
+# 010
+# 1010
+# 01010
+# 101010
+# 0101010
+# 10101010
+
+# n = int(input("Enter a Number of row :- "))
+
+# for i in range(n):
+#     for j in range(i):
+#         print(i,end="")
+#     print()
+
+# Enter a Number of row :- 8
+
+# 1
+# 22
+# 333
+# 4444
+# 55555
+# 666666
+# 7777777
+
+# n = int(input("Enter a Number of row :- "))
+
+# for i in range(n):
+#     for j in range(i):
+#         print(j,end="")
+#     print()
+
+# Enter a Number of row :- 8
+
+# 0
+# 01
+# 012
+# 0123
+# 01234
+# 012345
+# 0123456
+
+# n = int(input("Enter a Number of row :- "))
+
+# for i in range(n):
+#     for j in range(i+1,0,-1):
+#         print(j,end="")
+#     print()
+
+# Enter a Number of row :- 8
+# 1
+# 21
+# 321
+# 4321
+# 54321
+# 654321
+# 7654321
+# 87654321
+
+
+# n = int(input("Enter a Number of row :- "))
+
+# count = 0
+# for i in range(n):
+#     for j in range(i):
+#         print(count," ",end="")
+#         count+=1
+#     print()
+
+# Enter a Number of row :- 7
+
+# 0
+# 1  2
+# 3  4  5
+# 6  7  8  9
+# 10  11  12  13  14
+# 15  16  17  18  19  20
+
+# n = int(input("Enter a Number of row :- "))
+
+# for i in range(n):
+#     for j in range(i+1,0,-1):
+#         print(chr(65 + i),end="")
+#     print()
+
+# Enter a Number of row :- 8
+# A
+# BB
+# CCC
+# DDDD
+# EEEEE
+# FFFFFF
+# GGGGGGG
+# HHHHHHHH
+
+# n = int(input("Enter a Number of row :- "))
+
+# for i in range(n):
+#     for j in range(i):
+#         print(chr(65 + j),end="")
+#     print()
+
+# Enter a Number of row :- 8
+
+# A
+# AB
+# ABC
+# ABCD
+# ABCDE
+# ABCDEF
+# ABCDEFG
+
+# n = int(input("Enter a Number of row :- "))
+
+# for i in range(n):
+#     for j in range(i+1,0,-1):
+#         print(chr(64 + j),end="")
+#     print()
+
+# Enter a Number of row :- 8
+
+# A
+# BA
+# CBA
+# DCBA
+# EDCBA
+# FEDCBA
+# GFEDCBA
+# HGFEDCBA 
+
+# n = int(input("Enter a Number of row :- "))
+
+# count = 0
+# for i in range(n):
+#     for j in range(i):
+#         print(chr(65 + count)," ",end="")
+#         count+=1
+#     print()
+
+# Enter a Number of row :- 6
+
+# A
+# B  C
+# D  E  F
+# G  H  I  J
+# K  L  M  N  O
+
+
+# n = int(input("Enter a Number of row :- "))
+
+# for i in range(n):
+#     for j in range(n,i,-1):
+#         print(i,end="")
+#     print()
+
+# Enter a Number of row :- 8
+# 00000000
+# 1111111
+# 222222
+# 33333
+# 4444
+# 555
+# 66
+# 7
+
+# n = int(input("Enter a Number of row :- "))
+
+# for i in range(n):
+#     for j in range(n,i,-1):
+#         print(j,end="")
+#     print()
+
+# Enter a Number of row :- 8
+# 87654321
+# 8765432
+# 876543
+# 87654
+# 8765
+# 876
+# 87
+# 8
+
+# n = int(input("Enter a Number of row :- "))
+# count = 0
+# for i in range(n):
+#     for j in range(n,i,-1):
+#         print(count," ",end="")
+#         count+=1
+#     print()
+
+# Enter a Number of row :- 8
+# 0  1  2  3  4  5  6  7
+# 8  9  10  11  12  13  14
+# 15  16  17  18  19  20
+# 21  22  23  24  25
+# 26  27  28  29
+# 30  31  32
+# 33  34
+# 35
+
+# n = int(input("Enter a Number of row :- "))
+
+# for i in range(n,0,-1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+# Enter a Number of row :- 8
+# 12345678
+# 1234567
+# 123456
+# 12345
+# 1234
+# 123
+# 12
+# 1
+
+# n = int(input("Enter a Number of row :- "))
+
+# for i in range(n):
+#     for j in range(n,i,-1):
+#         print(chr(65+i),end="")
+#     print()
+
+# Enter a Number of row :- 8
+# AAAAAAAA
+# BBBBBBB
+# CCCCCC
+# DDDDD
+# EEEE
+# FFF
+# GG
+# H
+
